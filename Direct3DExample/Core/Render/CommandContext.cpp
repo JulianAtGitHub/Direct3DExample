ff4ece61242286cce4372d875966df11135377d6
Julian <zw616636719@gmail.com>
2019-05-21 20:31:17 +0800

    Add resource allocator to CommandContext

--- Direct3DExample/Core/Render/CommandContext.cpp
+++ Direct3DExample/Core/Render/CommandContext.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "CommandContext.h"
 #include "CommandQueue.h"
+#include "LinerAllocator.h"
 
 namespace Render {
 
@@ -11,6 +12,9 @@ CommandContext::CommandContext(ID3D12Device *device, const D3D12_COMMAND_LIST_TY
 , mCommandList(nullptr)
 , mCommandAlloctor(nullptr)
 , mFenceValue(0)
+, mCpuAllocator(nullptr)
+, mGpuAllocator(nullptr)
+, mResourceBarriers(MAX_RESOURCE_BARRIER)
 {
     Initialize();
 }
@@ -21,16 +25,66 @@ CommandContext::~CommandContext(void) {
 
 void CommandContext::Initialize(void) {
     mQueue = new CommandQueue(mDevice, mType);
-    mCommandAlloctor = mQueue->QueryAllocator();
-    ASSERT_SUCCEEDED(mDevice->CreateCommandList(0, mType, mCommandAlloctor, nullptr, IID_PPV_ARGS(&mCommandList)));
+
+    mCpuAllocator = new LinerAllocator(mDevice, LinerAllocator::CpuWritable);
+    mGpuAllocator = new LinerAllocator(mDevice, LinerAllocator::GpuExclusive);
 }
 
 void CommandContext::Destroy(void) {
+    mCommandList->Close();
+
     mQueue->DiscardAllocator(mCommandAlloctor, mFenceValue);
     mCommandAlloctor = nullptr;
     DeleteAndSetNull(mQueue);
 
     ReleaseAndSetNull(mCommandList);
+
+    DeleteAndSetNull(mCpuAllocator);
+    DeleteAndSetNull(mGpuAllocator);
+}
+
+void CommandContext::Begin(void) {
+    mCommandAlloctor = mQueue->QueryAllocator();
+    if (mCommandList) {
+        mCommandList->Reset(mCommandAlloctor, nullptr);
+    } else {
+        ASSERT_SUCCEEDED(mDevice->CreateCommandList(0, mType, mCommandAlloctor, nullptr, IID_PPV_ARGS(&mCommandList)));
+    }
+}
+
+void CommandContext::End(bool flush) {
+    if (!mCommandList) {
+        return;
+    }
+
+    FlushResourceBarriers();
+
+    uint64_t fenceValue = mQueue->ExecuteCommandList(mCommandList);
+    mQueue->DiscardAllocator(mCommandAlloctor, fenceValue);
+    mCommandAlloctor = nullptr;
+
+    uint64_t completeValue = mQueue->UpdateCompleteFence();
+    mCpuAllocator->CleanupPages(fenceValue, completeValue);
+    mGpuAllocator->CleanupPages(fenceValue, completeValue);
+
+    if (flush) {
+        mQueue->WaitForFence(fenceValue);
+    }
+}
+
+void CommandContext::AddResourceBarrier(D3D12_RESOURCE_BARRIER &barrier) {
+    mResourceBarriers.PushBack(barrier);
+    if (mResourceBarriers.Count >= MAX_RESOURCE_BARRIER) {
+        FlushResourceBarriers();
+    }
+}
+
+void CommandContext::FlushResourceBarriers(void) {
+    if (mResourceBarriers.Count() > 0) {
+        mCommandList->ResourceBarrier(mResourceBarriers.Count(), mResourceBarriers.Data());
+        mResourceBarriers.Clear();
+    }
+
 }
 
 }
--- Direct3DExample/Core/Render/CommandContext.h
+++ Direct3DExample/Core/Render/CommandContext.h
@@ -3,22 +3,34 @@
 namespace Render {
 
 class CommandQueue;
+class LinerAllocator;
 
 class CommandContext {
 public:
     CommandContext(ID3D12Device *device, const D3D12_COMMAND_LIST_TYPE type);
     ~CommandContext(void);
 
+    void Begin(void);
+    void End(bool flush = false);
+
 private:
     void Initialize(void);
     void Destroy(void);
 
+    void AddResourceBarrier(D3D12_RESOURCE_BARRIER &barrier);
+    void FlushResourceBarriers(void);
+
+    const static uint32_t MAX_RESOURCE_BARRIER = 16;
+
     const D3D12_COMMAND_LIST_TYPE   mType;
     ID3D12Device                   *mDevice;
     CommandQueue                   *mQueue;
-    ID3D12CommandList              *mCommandList;
+    ID3D12GraphicsCommandList      *mCommandList;
     ID3D12CommandAllocator         *mCommandAlloctor;
     uint64_t                        mFenceValue;
+    LinerAllocator                 *mCpuAllocator;
+    LinerAllocator                 *mGpuAllocator;
+    CList<D3D12_RESOURCE_BARRIER>   mResourceBarriers;
 };
 
 }
