c5f2f08a12e916897d10e3f677d63ea1487725c8
Julian <zw616636719@gmail.com>
2019-08-02 08:51:57 +0800

    Add cameraU, cameraV and cameraW

--- Framework/Utils/Camera.cpp
+++ Framework/Utils/Camera.cpp
@@ -3,18 +3,48 @@
 
 namespace Utils {
 
-Camera::Camera(float fov, float aspectRatio, float near, float far, const XMFLOAT4 &eye, const XMFLOAT4 &lookAt, const XMFLOAT4 &right)
+Camera::Camera(float fov, float aspectRatio, float near, float far, const XMFLOAT4 &eye, const XMFLOAT4 &lookAt)
+: mFov(fov)
+, mAspectRatio(aspectRatio)
+, mNear(near)
+, mFar(near)
 {
     mPosition = XMLoadFloat4(&eye);
     mDirection = XMVector3Normalize(XMLoadFloat4(&lookAt) - mPosition);
-    mRight = XMVector3Normalize(XMLoadFloat4(&right));
     mUp = { 0.0f, 1.0f, 0.0f, 0.0f };
+    mRight = XMVector3Normalize(XMVector3Cross(mDirection, mUp));
+    mProject = XMMatrixPerspectiveFovRH(fov, aspectRatio, near, far);
+    UpdateMatrixs();
+}
+
+Camera::Camera(float fov, float aspectRatio, float near, float far, const XMFLOAT4 &eye, const XMFLOAT4 &lookAt, const XMFLOAT4 &up)
+: mFov(fov)
+, mAspectRatio(aspectRatio)
+, mNear(near)
+, mFar(near)
+{
+    mPosition = XMLoadFloat4(&eye);
+    mDirection = XMVector3Normalize(XMLoadFloat4(&lookAt) - mPosition);
+    mUp = XMVector3Normalize(XMLoadFloat4(&up));
+    mRight = XMVector3Normalize(XMVector3Cross(mDirection, mUp));
     mProject = XMMatrixPerspectiveFovRH(fov, aspectRatio, near, far);
     UpdateMatrixs();
 }
 
 Camera::~Camera(void) {
 
+}
+
+void Camera::UpdateMatrixs(void) { 
+    mView = XMMatrixLookToRH(mPosition, mDirection, mUp); 
+
+    mW = XMVectorScale(mDirection, mFar);
+
+    mU = XMVector3Normalize(XMVector3Cross(mW, mUp));
+    mU = XMVectorScale(mU, mFar * tanf(mFov * 0.5f) * mAspectRatio);
+
+    mV = XMVector3Normalize(XMVector3Cross(mU, mW));
+    mV = XMVectorScale(mV, mFar * tanf(mFov * 0.5f));
 }
 
 }
--- Framework/Utils/Camera.h
+++ Framework/Utils/Camera.h
@@ -5,30 +5,47 @@ namespace Utils {
 class Camera {
 public:
     Camera(float fov, float aspectRatio, float near, float far, 
-           const XMFLOAT4 &eye, const XMFLOAT4 &lookAt, const XMFLOAT4 &right);
+           const XMFLOAT4 &eye, const XMFLOAT4 &lookAt);
+    Camera(float fov, float aspectRatio, float near, float far, 
+           const XMFLOAT4 &eye, const XMFLOAT4 &lookAt, const XMFLOAT4 &up);
     ~Camera(void);
 
     INLINE XMVECTOR GetPosition(void) const { return mPosition; };
+    INLINE XMVECTOR GetDirection(void) const { return mDirection; };
+    INLINE XMVECTOR GetUp(void) const { return mUp; };
+
+    INLINE XMVECTOR GetU(void) const { return mU; };
+    INLINE XMVECTOR GetV(void) const { return mV; };
+    INLINE XMVECTOR GetW(void) const { return mW; };
 
     INLINE XMMATRIX GetCombinedMatrix(void) { return mView * mProject; }
     INLINE XMMATRIX GetViewMatrix(void) const { return mView; }
     INLINE XMMATRIX GetProjectMatrix(void) const { return mProject; }
 
-    INLINE void UpdateMatrixs(void) { mView = XMMatrixLookToRH(mPosition, mDirection, mUp); }
-
     INLINE void MoveForward(float distance) { mPosition = mPosition + (distance * mDirection); }
     INLINE void MoveRight(float distance) { mPosition = mPosition + (distance * mRight); }
+
+    void UpdateMatrixs(void);
 
     void RotateY(float angle);
     void RotateX(float angle);
 
 private:
+    float       mFov;
+    float       mAspectRatio;
+    float       mNear;
+    float       mFar;
+
     XMVECTOR    mPosition;
     XMVECTOR    mDirection;
     XMVECTOR    mUp;
     XMVECTOR    mRight;
     XMMATRIX    mView;
     XMMATRIX    mProject;
+
+    XMVECTOR    mU;
+    XMVECTOR    mV;
+    XMVECTOR    mW;
 };
 
 INLINE void Camera::RotateY(float angle) {
