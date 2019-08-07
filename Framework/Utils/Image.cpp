41bdbd3f4922dffb1ad367329aade3966fe527b6
Julian <zw616636719@gmail.com>
2019-08-08 03:41:27 +0800

    Class Image can load hdr file

--- Framework/Utils/Image.cpp
+++ Framework/Utils/Image.cpp
@@ -28,8 +28,6 @@ Image * Image::CreateFromFile(const char *filePath) {
         return nullptr;
     }
 
-    stbi_set_flip_vertically_on_load(1);
-
     if (stbi_is_hdr(filePath)) {
         return CreateHDRImage(filePath);
     } else {
@@ -38,6 +36,7 @@ Image * Image::CreateFromFile(const char *filePath) {
 }
 
 Image * Image::CreateBitmapImage(const char *filePath) {
+    stbi_set_flip_vertically_on_load(1);
     int width, height, channels;
     stbi_uc *pixels = stbi_load(filePath, &width, &height, &channels, STBI_rgb_alpha);
     if (!pixels) {
@@ -60,7 +59,32 @@ Image * Image::CreateBitmapImage(const char *filePath) {
 }
 
 Image * Image::CreateHDRImage(const char *filePath) {
-    return nullptr;
+    stbi_set_flip_vertically_on_load(0);
+    int width, height, channels;
+    float *pixels = stbi_loadf(filePath, &width, &height, &channels, STBI_default);
+    if (!pixels) {
+        return nullptr;
+    }
+
+    ASSERT_PRINT((channels == 3 || channels == 4));
+    uint32_t bpp = static_cast<uint32_t>(sizeof(float)) * channels;
+
+    Image *image = new Image();
+    image->mWidth = width;
+    image->mHeight = height;
+    image->mPitch = width * bpp;
+    image->mPixels = malloc(image->mPitch * image->mHeight);
+    if (channels == 3) {
+        image->mFormat = R32_G32_B32_FLOAT;
+    } else {
+        image->mFormat = R32_G32_B32_A32_FLOAT;
+    }
+    
+    memcpy(image->mPixels, pixels, image->mPitch * image->mHeight);
+
+    stbi_image_free(pixels);
+
+    return image;
 }
 
 
@@ -89,9 +113,9 @@ DXGI_FORMAT Image::GetDXGIFormat(void) {
         case R8_G8_B8_A8:
             return DXGI_FORMAT_R8G8B8A8_UNORM;
         case R32_G32_B32_FLOAT:
-            return DXGI_FORMAT_R32G32B32A32_FLOAT;
-        case R32_G32_B32_A32_FLOAT:
             return DXGI_FORMAT_R32G32B32_FLOAT;
+        case R32_G32_B32_A32_FLOAT:
+            return DXGI_FORMAT_R32G32B32A32_FLOAT;
         default:
             return DXGI_FORMAT_UNKNOWN; 
     }
