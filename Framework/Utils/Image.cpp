40dd2e5e4650eb67f6df64d428280d2f4ba68eab
Julian <zw616636719@gmail.com>
2019-08-08 02:55:32 +0800

    Add help class image to load image data from file

---
+++ Framework/Utils/Image.cpp
@@ -0,0 +1,100 @@
+#include "stdafx.h"
+#include "Image.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+namespace Utils {
+
+Image * Image::LoadFromBinary(const void *head, void *pixels) {
+    if (!head || !pixels) {
+        return nullptr;
+    }
+
+    Image *image = new Image();
+    const Image *other = (const Image *)head;
+    image->mWidth = other->mWidth;
+    image->mHeight = other->mHeight;
+    image->mPitch = other->mPitch;
+    image->mFormat = other->mFormat;
+    image->mPixels = malloc(image->mPitch * image->mHeight);
+    memcpy(image->mPixels, pixels, image->mPitch * image->mHeight);
+
+    return image;
+}
+
+Image * Image::CreateFromFile(const char *filePath) {
+    if (!filePath) {
+        return nullptr;
+    }
+
+    stbi_set_flip_vertically_on_load(1);
+
+    if (stbi_is_hdr(filePath)) {
+        return CreateHDRImage(filePath);
+    } else {
+        return CreateBitmapImage(filePath);
+    }
+}
+
+Image * Image::CreateBitmapImage(const char *filePath) {
+    int width, height, channels;
+    stbi_uc *pixels = stbi_load(filePath, &width, &height, &channels, STBI_rgb_alpha);
+    if (!pixels) {
+        return nullptr;
+    }
+
+    constexpr uint32_t bpp = 4;
+
+    Image *image = new Image();
+    image->mWidth = width;
+    image->mHeight = height;
+    image->mPitch = width * bpp;
+    image->mPixels = malloc(image->mPitch * image->mHeight);
+    image->mFormat = R8_G8_B8_A8;
+    memcpy(image->mPixels, pixels, image->mPitch * image->mHeight);
+
+    stbi_image_free(pixels);
+
+    return image;
+}
+
+Image * Image::CreateHDRImage(const char *filePath) {
+    return nullptr;
+}
+
+
+Image * Image::CreateCompressedImage(const char *filePath) {
+    return nullptr;
+}
+
+Image::Image(void)
+: mWidth(0)
+, mHeight(0)
+, mPitch(0)
+, mFormat(Unknown)
+, mPixels(nullptr)
+{
+
+}
+
+Image::~Image(void) {
+    if (mPixels) {
+        free(mPixels);
+    }
+}
+
+DXGI_FORMAT Image::GetDXGIFormat(void) {
+    switch (mFormat) {
+        case R8_G8_B8_A8:
+            return DXGI_FORMAT_R8G8B8A8_UNORM;
+        case R32_G32_B32_FLOAT:
+            return DXGI_FORMAT_R32G32B32A32_FLOAT;
+        case R32_G32_B32_A32_FLOAT:
+            return DXGI_FORMAT_R32G32B32_FLOAT;
+        default:
+            return DXGI_FORMAT_UNKNOWN; 
+    }
+}
+
+}
---
+++ Framework/Utils/Image.h
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace Utils {
+
+class Image {
+public:
+    enum Format {
+        Unknown,
+        R8_G8_B8_A8,
+        R32_G32_B32_FLOAT,
+        R32_G32_B32_A32_FLOAT,
+    };
+
+    static Image * LoadFromBinary(const void *head, void *pixels);
+    static Image * CreateFromFile(const char *filePath);
+
+    ~Image(void);
+
+    INLINE uint32_t GetWidth(void) const { return mWidth; }
+    INLINE uint32_t GetHeight(void) const { return mHeight; }
+    INLINE uint32_t GetPitch(void) const { return mPitch; }
+    INLINE Format GetFormat(void) const { return mFormat; }
+    INLINE const void * GetPixels(void) const { return mPixels; }
+    INLINE uint32_t GetPixelsSize(void) { return mPitch * mHeight; }
+
+    DXGI_FORMAT GetDXGIFormat(void);
+
+private:
+    static Image * CreateBitmapImage(const char *filePath);
+    static Image * CreateHDRImage(const char *filePath);
+    static Image * CreateCompressedImage(const char *filePath);
+
+    Image(void);
+
+    uint32_t    mWidth;
+    uint32_t    mHeight;
+    uint32_t    mPitch;
+    Format      mFormat;
+    void *      mPixels;
+};
+
+}
