0b01fed01c595a11b28a5e9dcce87691e269ea16
Julian <julian.plunkett@outlook.com>
2020-02-09 23:14:34 +0800

    [Solution] Remove unused project

--- Direct3DExample/Shaders/color.ps.hlsl
+++
@@ -1,9 +0,0 @@
-#include "inputs.hlsli"
-
-Texture2D g_texture : register(t0);
-SamplerState g_sampler : register(s0);
-
-float4 PSMain(PSInput input) : SV_TARGET
-{
-    return g_texture.Sample(g_sampler, input.uv);
-}
